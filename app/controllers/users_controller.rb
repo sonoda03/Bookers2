ceab44dcd9ac46879dbb930dd40401b2e5a1a0fb
EC2 Default User <ec2-user@ip-172-31-47-249.ap-northeast-1.compute.internal>
2022-09-20 13:08:16 +0000

    userの詳細画面記述途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,13 @@ class UsersController < ApplicationController
   end
   
   def show
-    
+    @user = User.find(params[:id])
+    @books = @user.books
   end
 
   
 
   def edit
+    @user = User.find(params[:id])
   end
 end
