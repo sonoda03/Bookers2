bf5ff0e93b7b67356db0827a818e254f32b1e666
EC2 Default User <ec2-user@ip-172-31-47-249.ap-northeast-1.compute.internal>
2022-09-20 04:12:15 +0000

    [Add]users controller追加(:index,show,edit)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   devise_for :users
   # get 'homes/top'
   root to: "homes#top"
@@ -7,6 +8,11 @@ Rails.application.routes.draw do
   # get 'books/index'
   # get 'books/show'
   # get 'books/edit'
-  resources :books, only:[:new, :create, :index, :show, :edit]
+  resources :books, only:[:new, :create, :index, :show, :edit, :destroy]
+  
+  # get 'users/show'
+  # get 'users/index'
+  # get 'users/edit'
+  resources :users, only:[:index, :show, :edit]
   
 end
